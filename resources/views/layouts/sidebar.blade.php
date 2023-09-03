952664ca80d88dde84e5437c7eab9693835314c5
mohamedalijinna <jinnasithiq@gmail.com>
2023-09-03 18:19:10 +0530

    common sidebar for admin and dasboard page

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
 <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
     <div class="position-sticky mt-5">
         <div class="list-group list-group-flush mx-3 mt-4">
-            <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+            <a href="{{ route('admin') }}" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
                 <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Main dashboard</span>
             </a>
             <a href="#" class="list-group-item list-group-item-action py-2 ripple">
