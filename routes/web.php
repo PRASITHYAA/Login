b06837d122153f61030d9188c637178e6b46ca9c
mohamedalijinna <jinnasithiq@gmail.com>
2023-09-03 22:26:52 +0530

    roles(Admin,User) and permission

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,6 @@ Route::get('logout', [LoginController::class, 'logout']);
 
 // dashboard
 Route::view('/home', 'auth.Home')->middleware('auth')->name('home');
-Route::view('/admin', 'admin')->middleware('auth')->name('admin');
 
 //  forget password form
 Route::get('forgetpassword', [ForgetPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
@@ -46,43 +45,57 @@ Route::post('forgetpassword', [ForgetPasswordController::class, 'submitForgetPas
 Route::get('reset-password/{token}', [ForgetPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
 Route::post('reset-password', [ForgetPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
 
+Route::group(['middleware' => ['auth', 'role:Admin']], function () {
+
+//dashboard
+    Route::view('/admin', 'admin')->middleware('auth')->name('admin');
 //  customer(user)
-Route::get('users', [UserController::class, 'index'])->name('users.index');
-Route::get('users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('users', [UserController::class, 'store'])->name('users.store');
-Route::get('users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::get('users', [UserController::class, 'index'])->name('users.index');
+    Route::get('users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('users', [UserController::class, 'store'])->name('users.store');
+    Route::get('users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
 // sector
-Route::get('/sector', [SectorController::class, 'index'])->name('sector.index');
-Route::get('/sector/create', [SectorController::class, 'create'])->name('sector.create');
-Route::post('/sector', [SectorController::class, 'store'])->name('sector.store');
-Route::get('/sector/{sector}', [SectorController::class, 'show'])->name('sector.show');
-Route::get('/sector/{sector}/edit', [SectorController::class, 'edit'])->name('sector.edit');
-Route::put('/sector/{sector}', [SectorController::class, 'update'])->name('sector.update');
-Route::delete('/sector/{sector}', [SectorController::class, 'destroy'])->name('sector.destroy');
+    Route::get('/sector', [SectorController::class, 'index'])->name('sector.index');
+    Route::get('/sector/create', [SectorController::class, 'create'])->name('sector.create');
+    Route::post('/sector', [SectorController::class, 'store'])->name('sector.store');
+    Route::get('/sector/{sector}', [SectorController::class, 'show'])->name('sector.show');
+    Route::get('/sector/{sector}/edit', [SectorController::class, 'edit'])->name('sector.edit');
+    Route::put('/sector/{sector}', [SectorController::class, 'update'])->name('sector.update');
+    Route::delete('/sector/{sector}', [SectorController::class, 'destroy'])->name('sector.destroy');
 
 // position
 
-Route::get('/position', [PositionController::class, 'index'])->name('position.index');
-Route::get('/position/create', [PositionController::class, 'create'])->name('position.create');
-Route::post('/position', [PositionController::class, 'store'])->name('position.store');
-Route::get('/position/{position}', [PositionController::class, 'show'])->name('position.show');
-Route::get('/position/{position}/edit', [PositionController::class, 'edit'])->name('position.edit');
-Route::put('/position/{position}', [PositionController::class, 'update'])->name('position.update');
-Route::delete('/position/{position}', [PositionController::class, 'destroy'])->name('position.destroy');
+    Route::get('/position', [PositionController::class, 'index'])->name('position.index');
+    Route::get('/position/create', [PositionController::class, 'create'])->name('position.create');
+    Route::post('/position', [PositionController::class, 'store'])->name('position.store');
+    Route::get('/position/{position}', [PositionController::class, 'show'])->name('position.show');
+    Route::get('/position/{position}/edit', [PositionController::class, 'edit'])->name('position.edit');
+    Route::put('/position/{position}', [PositionController::class, 'update'])->name('position.update');
+    Route::delete('/position/{position}', [PositionController::class, 'destroy'])->name('position.destroy');
 
 // energy
 
-Route::get('/energy', [EnergyController::class, 'index'])->name('energy.index');
-Route::get('/energy/create', [EnergyController::class, 'create'])->name('energy.create');
-Route::post('/energy', [EnergyController::class, 'store'])->name('energy.store');
-Route::get('/energy/{energy}', [EnergyController::class, 'show'])->name('energy.show');
-Route::get('/energy/{energy}/edit', [EnergyController::class, 'edit'])->name('energy.edit');
-Route::put('/energy/{energy}', [EnergyController::class, 'update'])->name('energy.update');
-Route::delete('/energy/{energy}', [EnergyController::class, 'destroy'])->name('energy.destroy');
+    Route::get('/energy', [EnergyController::class, 'index'])->name('energy.index');
+    Route::get('/energy/create', [EnergyController::class, 'create'])->name('energy.create');
+    Route::post('/energy', [EnergyController::class, 'store'])->name('energy.store');
+    Route::get('/energy/{energy}', [EnergyController::class, 'show'])->name('energy.show');
+    Route::get('/energy/{energy}/edit', [EnergyController::class, 'edit'])->name('energy.edit');
+    Route::put('/energy/{energy}', [EnergyController::class, 'update'])->name('energy.update');
+    Route::delete('/energy/{energy}', [EnergyController::class, 'destroy'])->name('energy.destroy');
 
+    //  training
+    Route::get('/training', [TrainingController::class, 'index'])->name('training.index');
+    Route::get('/training/create', [TrainingController::class, 'create'])->name('training.create');
+    Route::post('/training', [TrainingController::class, 'store'])->name('training.store');
+    Route::get('/training/{training}', [TrainingController::class, 'show'])->name('training.show');
+    Route::get('/training/{training}/edit', [TrainingController::class, 'edit'])->name('training.edit');
+    Route::put('/training/{training}', [TrainingController::class, 'update'])->name('training.update');
+    Route::delete('/training/{training}', [TrainingController::class, 'destroy'])->name('training.destroy');
+
+});
 // training
 
  // card
@@ -92,15 +105,6 @@ Route::delete('/energy/{energy}', [EnergyController::class, 'destroy'])->name('e
  //  education
  Route::view('/education', 'career.education.create')->name('education.view');
  Route::post('education', [EducationController::class, 'store'])->name('education.store');
-
-//  training
-Route::get('/training', [TrainingController::class, 'index'])->name('training.index');
-Route::get('/training/create', [TrainingController::class, 'create'])->name('training.create');
-Route::post('/training', [TrainingController::class, 'store'])->name('training.store');
-Route::get('/training/{training}', [TrainingController::class, 'show'])->name('training.show');
-Route::get('/training/{training}/edit', [TrainingController::class, 'edit'])->name('training.edit');
-Route::put('/training/{training}', [TrainingController::class, 'update'])->name('training.update');
-Route::delete('/training/{training}', [TrainingController::class, 'destroy'])->name('training.destroy');
 
 // career home page
 Route::view('/career_home', 'career.home');
