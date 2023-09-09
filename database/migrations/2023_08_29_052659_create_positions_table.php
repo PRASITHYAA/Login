e11e47c9e60202f60adbc70a6995aeebf65ca223
prasithya <prasithyaaaa@gmail.com>
2023-09-09 15:00:54 +0530

    position table ,fixes

--- database/migrations/2023_08_29_052659_create_positions_table.php
+++ database/migrations/2023_08_29_052659_create_positions_table.php
@@ -16,7 +16,6 @@ return new class extends Migration {
             $table->id();
             $table->unsignedBigInteger('sector_id');
             $table->string('name');
-            // $table->string('description');
             $table->timestamps();
             $table->foreign('sector_id')->references('id')->on('sectors');
         });
