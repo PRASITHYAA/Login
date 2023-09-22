e1cd2a7e9437150f1d2ddaf74fced3ad12a59e41
prasithya <prasithyaaaa@gmail.com>
2023-09-22 12:32:27 +0530

    user_sidebar UI fixes

--- resources/views/layouts/front/user_sidebar.blade.php
+++ resources/views/layouts/front/user_sidebar.blade.php
@@ -1,3 +1,29 @@
+<style>
+            .scroll-container {
+            width: 100%;
+            overflow-x: scroll;
+            white-space: nowrap;
+            border: 1px solid #ccc;
+        }
+
+        .scroll-content {
+            display: inline-block;
+            padding: 10px;
+        }
+
+        body {
+            overflow-x: hidden;
+        }
+        .side-nav {
+            color: black;
+            font-weight: bold;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+    </style>
+
+
+
 <div class="col-lg-3 pt-4 ">
     <div class="container  border 2px solid black">
         <i class="fas fa-tachometer-alt fa-fw me-3"></i> <a class="side-nav" href="{{ route('dashboard') }}">
