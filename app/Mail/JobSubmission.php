1d0fb398caf994ca608a78d76b247ed922d6c732
mohamedalijinna <jinnasithiq@gmail.com>
2023-10-16 20:00:32 +0530

    Pdf attachment in email

--- app/Mail/JobSubmission.php
+++ app/Mail/JobSubmission.php
@@ -54,7 +54,7 @@ class JobSubmission extends Mailable
     {
 
         return [
-            Attachment::fromPath('storage/app/public/'.$this->data['first_name'].'.pdf')
+            Attachment::fromPath('storage/app/public/job_application_'.$this->data['first_name'].'.pdf')
                 ->as($this->data['first_name'].'.pdf')
                 ->withMime('application/pdf'),
         ];
