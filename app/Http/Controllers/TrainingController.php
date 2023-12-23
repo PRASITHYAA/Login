f27c54f137ee668de78f2d01f5389fa04c35e02a
mohamedalijinna <jinnasithiq@gmail.com>
2023-12-23 16:13:27 +0530

    training submission email issue fixed

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -85,7 +85,7 @@ class TrainingController extends Controller
         $this->savePdf($training);
         $emailData['view'] = 'mail.training_submission';
         Mail::to(env('EMAIL_TO', $training->primary_email))->send(new TrainingSubmission($emailData));
-        $emailData['view'] = 'mail.job_submission_admin';
+        $emailData['view'] = 'mail.training_submission_admin';
         Mail::to('info@tisecon.com')->send(new TrainingSubmission($emailData));
         session()->flash('success', 'Training form submitted successfully.');
         return redirect()->route('dashboard');
@@ -122,9 +122,9 @@ class TrainingController extends Controller
         $emailData['course_title'] = $training->course_title->name;
         $emailData['created_at'] = Carbon::parse($training->created_at)->format('d_m_Y_h_i_A');;
         $this->savePdf($training);
-        $emailData['view'] = 'mail.job_submission';
+        $emailData['view'] = 'mail.training_submission';
         Mail::to(env('EMAIL_TO', $training->primary_email))->send(new TrainingSubmission($emailData));
-        $emailData['view'] = 'mail.job_submission_admin';
+        $emailData['view'] = 'mail.training_submission_admin';
         Mail::to('info@tisecon.com')->send(new TrainingSubmission($emailData));
         session()->flash('success', 'Training form updated successfully.');
         return redirect()->route('dashboard');
